91ba3bd04055837b88f130495e28bc8b7ec02ad9
sophsegu <soph.segu@gmail.com>
2026-01-05 00:29:37 -0500

    Checks if there is a hand present on screen, also create the Hand Data Class

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import cv2
+import HandData
 
 #Global variables
 background = None
-hand = None
+hand = HandData.HandData(0,0,0,0,0)
 frames_elapsed = 0
 FRAME_HEIGHT = 500
 FRAME_WIDTH = 750
@@ -12,12 +13,36 @@ CALIBRATION_TIME = 30
 BG_WEIGHT = 0.5
 OBJ_THRESHOLD = 18
 
-def main():
-    region_top = 0
+# Here we take the current frame, the number of frames elapsed, and how many fingers we've detected
+# so we can print on the screen which gesture is happening (or if the camera is calibrating).
+def write_on_image(frame, hand, frames_elapsed):
+    region_top    = 0
     region_bottom = int(2 * FRAME_HEIGHT / 3)
-    region_left = int(FRAME_WIDTH / 2)
-    region_right = FRAME_WIDTH
+    region_left   = int(FRAME_WIDTH / 2)
+    region_right  = FRAME_WIDTH
 
+    hand.update(region_top, region_bottom, region_left, region_right)
+    if frames_elapsed < CALIBRATION_TIME:
+        text = "Calibrating..."
+    elif hand == None or not hand.isInFrame:
+        text = "No hand detected"
+    else:
+        if hand.isWaving:
+            text = "Waving"
+        elif hand.fingers == 0:
+            text = "Rock"
+        elif hand.fingers == 1:
+            text = "Pointing"
+        elif hand.fingers == 2:
+            text = "Scissors"
+    
+    cv2.putText(frame, text, (10,20), cv2.FONT_HERSHEY_COMPLEX, 0.4,( 0 , 0 , 0 ),2,cv2.LINE_AA)
+    cv2.putText(frame, text, (10,20), cv2.FONT_HERSHEY_COMPLEX, 0.4,(255,255,255),1,cv2.LINE_AA)
+
+    # Highlight the region of interest using a drawn rectangle.
+    cv2.rectangle(frame, (region_left, region_top), (region_right, region_bottom), (255,255,255), 2)
+
+def main():
     frames_elapsed = 0
     capture = cv2.VideoCapture(0)
 
@@ -27,7 +52,9 @@ def main():
         frame = cv2.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))
         # Flip the frame over the vertical axis so that it works like a mirror, which is more intuitive to the user.
         frame = cv2.flip(frame, 1)
-
+        
+        # Write the action the hand is doing on the screen, and draw the region of interest.
+        write_on_image(frame, hand, frames_elapsed)
         cv2.imshow("Camera Input", frame)
         frames_elapsed += 1
         # Check if user wants to exit.
